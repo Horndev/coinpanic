81d5a8ca0d5d8dece6f690dbac976b491e401a95
Steven Horn <horn@horndev.com>
2018-01-07 13:35:27 -0400

    Adding content - fonts, donation address

--- coinpanic_airdrop/App_Start/BundleConfig.cs
+++ coinpanic_airdrop/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace coinpanic_airdrop
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/icons").Include(
+                        "~/bower_components/webicon/jquery-webicon.min.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -25,6 +28,7 @@ namespace coinpanic_airdrop
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/bower_components/font-awesome/css/font-awesome.min.css",
                       "~/Content/site.css"));
         }
     }
