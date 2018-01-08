cc57f1c799bed2135ea10fc9d32ae8fcc7e72674
Steven Horn <horn@horndev.com>
2018-01-07 20:57:02 -0400

    Adding Claim Forms

---
+++ coinpanic_airdrop/Controllers/ClaimController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace coinpanic_airdrop.Controllers
+{
+    public class ClaimController : Controller
+    {
+        // GET: Claim
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BCD()
+        {
+            return View();
+        }
+    }
+}
