c01c494d68e0d33801c26a312b974c342faa3432
Steven Horn <horn@horndev.com>
2018-05-03 20:19:41 -0300

    Removed bower package bitcore-lib

--- coinpanic_airdrop/App_Start/BundleConfig.cs
+++ coinpanic_airdrop/App_Start/BundleConfig.cs
@@ -32,9 +32,6 @@ namespace coinpanic_airdrop
             bundles.Add(new ScriptBundle("~/bundles/icons").Include(
                         "~/bower_components/webicon/jquery-webicon.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/cryptolib").Include(
-                       "~/bower_components/bitcore-lib/bitcore-lib.js"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
